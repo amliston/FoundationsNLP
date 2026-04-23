d793c5bf4687c208a2b29ef769af585bcccb430d
Alexis Liston <lexil@hotmail.com>
2026-04-23 15:10:33 +0200

    combined config exp and plot

---
+++ config/train_shakespeare_char_combineconfig.py
@@ -0,0 +1,37 @@
+# Experiment 5: Combining Configurations
+# Change: as described above
+# All other hyperparameters match baseline
+
+out_dir = 'out-shakespeare-combineconfig'
+
+eval_interval = 250
+eval_iters = 200            
+log_interval = 10
+
+always_save_checkpoint = False
+
+wandb_log = False
+wandb_project = 'nanoGPT-assignment'
+wandb_run_name = 'combineconfig'
+
+dataset = 'shakespeare_char'
+gradient_accumulation_steps = 1
+batch_size = 64
+block_size = 64         # CHANGED: baseline was 256
+
+n_layer = 2         # CHANGED: baseline was 6              
+n_head = 6
+n_embd = 384
+dropout = 0.4    # CHANGED: baseline was 0.2
+
+learning_rate = 5e-4       # CHANGED: baseline was 1e-3    
+max_iters = 3000            #CHANGED: baseline was 5000
+lr_decay_iters = 3000       #CHANGED: baseline was 5000
+min_lr = 1e-4
+beta2 = 0.99
+
+warmup_iters = 100
+weight_decay = 1e-1
+
+device = 'cuda' # change to 'cpu' for CPU
+compile = False
